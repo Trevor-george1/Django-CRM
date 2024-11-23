cc519cb8bcd00d3bd042b6dc81600d6fe50cef78
Trevor-george1 <Trevormahinda@gmail.com>
2024-11-23 13:32:08 +0300

    final

--- website/views.py
+++ website/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+
+    records = Record.objects.all()
+
+
+
     #check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,7 +26,7 @@ def home(request):
             messages.success(request, "Not valid username...")
             return redirect('home')
 
-    return render(request, 'home.html', {})
+    return render(request, 'home.html', {'records': records})
 
 
 def logout_user(request):
@@ -27,3 +34,44 @@ def logout_user(request):
     messages.success(request, "You have Logged out...")
     return redirect('home')
 
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # AAUTHENTICATE 
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=username, password=password)
+
+            login(request, user)
+            messages.success(request, "You have successfully registered")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
+
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # lookup record
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    
+    else:
+        messages.success(request, "You must be logged in")
+        return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+
+        delete_it.delete()
+        messages.success(request, "Record deleted successful")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in")
+        return redirect('home')
